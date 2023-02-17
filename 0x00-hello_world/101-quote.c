86990ab9d34c8972e790b14d0cc022b7f321fd7b
123BIRDY <fhevi39@gmail.com>
2023-02-17 06:57:36 -0800

    Write C program that prints without using printf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
 /**
  * main - describes entry level
  *
- * Return: 1 if successful
+ * Return: (1) if successful
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 45);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return 1;
 }	
