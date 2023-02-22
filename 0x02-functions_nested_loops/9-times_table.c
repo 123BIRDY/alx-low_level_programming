f6e77c4a23b6ce51c5082516d5308de8a1a34cac
123BIRDY <fhevi39@gmail.com>
2023-02-22 08:14:59 -0800

    deletes files

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,40 +2,34 @@
 
 /**
  ** times_table - Entry Point
- * Description: Prints 9 times table
- * Return: void
+ *
  */
 
 void times_table(void)
 {
-	int i, j, ans, tens, unit;
+	int num, mult, prod;
 
-	for (i = 0; i <= 9; i++)
+	for (num = 0; num <= 9; num++)
 	{
-		for (j = 0; j <= 9; j++)
+		_putchar('0');
+	}
+
+	for (mult = 1; mult <= 9; mult++)
+	{
+		_putchar(',');
+		_putchar(' ');
+	}
+
+		prod = num * mult;
+
+	if (prod <= 9)
+		_putchar(num + '0');
+	else
 		{
-			ans = i * j;
-			tens = ans / 10;
-			unit = ans % 10;
-			if (j == 0)
-			{
-				_putchar('0');
-			}
-			else if (ans < 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(ans + ' ');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(tens / 10 + '0');
-				_putchar(unit % 10 + '0');
-			}
+			_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
 		}
-		_putchar('\n');
+	}
+	_putchar('\n');
 	}
 }
