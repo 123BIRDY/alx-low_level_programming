bd7fbffa86448bb4712b39ce721047c6a43a61fa
123BIRDY <fhevi39@gmail.com>
2023-06-16 06:11:49 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints a line using the puts function
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
