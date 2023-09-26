0a4ba1e97c676730f8e55ccd75877c78a37dde71
123BIRDY <fhevi39@gmail.com>
2023-09-26 07:59:28 -0700

    Print list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -19,8 +19,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 /* Prototypes */
@@ -40,3 +40,5 @@ listint_t *add_nodeint(listint_t **head, const int n);
 size_t listint_len(const listint_t *h);
 size_t print_listint(const listint_t *h);
 int _putchar(char c);
+
+#endif
