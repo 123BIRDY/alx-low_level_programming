c5cde2d42b31c649d46d4d165fcce0d241865931
123BIRDY <fhevi39@gmail.com>
2023-09-27 07:04:42 -0700

    Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,31 +9,31 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current, *vets;
-	size_t counter = 0;
+	const listint_t *now, *safe;
+	size_t count = 0;
 
 	if (!head || !head->next)
 	{
 		return (0);
 	}
-	/* Traverse the linke list */
-	current = head;
-	while (current)
+	/* Move through the linked list */
+	now = head;
+	while (now)
 	{
-		/* Print current node */
-		printf("[%p] %d\n", (void *)current, current->n);
-		counter++;
+		/* Print the present node */
+		printf("[%p] %d\n", (void *)now, now->n);
+		count++;
 		/* Break loop if next pointer points to previous node */
-		if (current > current->next)
+		if (now > now->next)
 		{
-			current = current->next;
+			now = now->next;
 		}
 		else
 		{
-			vets = current->next;
-			printf("-> [%p] %d\n", (void *)vets, vets->n);
+			safe = now->next;
+			printf("-> [%p] %d\n", (void *)safe, safe->n);
 			break;
 		}
 	}
-	return (counter);
+	return (count);
 }
